1264e9bf4f47704bf4b96900de2282e30d62b146
silesnet <>
2014-04-10 10:26:34 +0200

    added test runner errorlevel checking

--- src/test/vbs/run-tests.cmd
+++ src/test/vbs/run-tests.cmd
@@ -1,7 +1,13 @@
 @echo off
 cscript.exe /Nologo loader-test.vbs
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cscript.exe /Nologo insert-gt-test.vbs
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cscript.exe /Nologo journal-test.vbs
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cscript.exe /Nologo yaml-parser-test.vbs
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cscript.exe /Nologo ..\..\main\vbs\load-to-insert-gt.vbs fixtures\yaml-fixture.yml subiekt.xml invoices tmp\invoices.jrn --dry
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cscript.exe /Nologo ..\..\main\vbs\load-to-insert-gt.vbs fixtures\yaml-fixture.yml subiekt.xml customers tmp\customers.jrn --dry
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
